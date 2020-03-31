768d7a319408210d63477040e3887ea58da24f3e
Jakub Waszkiewicz <qbazilla@hotmail.com>
2020-03-31 11:13:43 +0200

    add dataset loader using torch

---
+++ datasets.py
@@ -0,0 +1,12 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+def load_CIFAR10():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trainset = torchvision.datasets.CIFAR10(root='./cifar-dataset', train=True, download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR10(root='./cifar-dataset', train=False, download=True, transform=transform)
+    return trainset, testset
