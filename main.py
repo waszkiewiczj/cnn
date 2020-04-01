1036335ab66634c2332846f89eb93db196530808
qbazilla <qbazilla@hotmail.com>
2020-04-01 20:26:29 +0200

    update main to load kaggle data

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from torch import nn
-
 import observers
 import train
 import test
-import kaggle
-from datasets import load_CIFAR10
-from networks import TestNet
+import datasets.cifar10
+import networks
 
 
 def main():
-    trainset, testset = load_CIFAR10()
+    trainset, testset = datasets.cifar10.from_kaggle()
     batch_size = 4
     epochs = 2
     lr = 0.001
@@ -17,18 +15,16 @@ def main():
     criterion = nn.CrossEntropyLoss()
     seed = 1000
     observer = observers.DummyPrintObserver()
-    net = TestNet()
+    net = networks.TestNet()
 
     train_config = train.TrainConfig(trainset=trainset, batch_size=batch_size, epochs=epochs, lr=lr, momentum=momentum,
                                criterion=criterion, seed=seed)
 
     train.train_network(network=net, config=train_config, observer=observer)
 
-    predicted = test.predict(net, testset)
-    accuracy = test.get_accuracy(predicted, testset.targets)
+    predicted = test.predict(net, trainset)
+    accuracy = test.get_accuracy(predicted, trainset.targets)
     print('accuracy: %.2f' % accuracy)
-    submission_df = kaggle.create_submission_df(testset, predicted)
-    submission_df.to_csv('kaggle_submissions/first-try-sub.csv', index=False)
 
 
 if __name__ == '__main__':
