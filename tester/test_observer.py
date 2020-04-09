d4149234ec109d8911de08eb6d4d18efed0a3093
qbazilla <qbazilla@hotmail.com>
2020-04-09 23:53:45 +0200

    add testing observer

---
+++ tester/test_observer.py
@@ -0,0 +1,30 @@
+from trainer.observers import ConstFreqObserver
+from tester.predict import predict
+from tester.test import get_accuracy
+import pandas as pd
+
+
+class TestObserver(ConstFreqObserver):
+    def __init__(self, freq, validation_set):
+        super(ConstFreqObserver, self).__init__(freq)
+        self.validation_set = validation_set
+        self.data = pd.DataFrame({
+            'epoch': [],
+            'accuracy': [],
+            'loss': []
+        })
+
+    def freq_update(self, network, epoch, iteration, loss):
+        predicted = predict(network, self.validation_set)
+        acc = get_accuracy(predicted, self.validation_set.target)
+        self.data.append({
+            'epoch': [epoch],
+            'accuracy': [acc],
+            'loss': [loss]
+        })
+
+    def get_results(self):
+        grouped = self.data.groupby('epoch')
+        means = grouped.mean().add_prefix('mean_')
+        vars = grouped.var().add_prefix('var_')
+        return means.join(vars).reset_index()
