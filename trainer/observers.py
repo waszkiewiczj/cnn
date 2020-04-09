009f9f9fd7feff319acb14d6236a78728047c1ea
qbazilla <qbazilla@hotmail.com>
2020-04-09 20:35:02 +0200

    add constant frequency observer

--- trainer/observers.py
+++ trainer/observers.py
@@ -18,3 +18,15 @@ class DummyPrintObserver(TrainingObserver):
     def update(self, network, epoch, iteration, loss):
         if not iteration % 50:
             print(f'epoch - {epoch}, iteration - {iteration}, loss - {loss}')
+
+
+class ConstFreqObserver(TrainingObserver):
+    def __init__(self, freq):
+        self.freq = freq
+
+    def update(self, network, epoch, iteration, loss):
+        if epoch % self.freq == 0:
+            self.freq_update(network, epoch, iteration, loss)
+
+    def freq_update(self, network, epoch, iteration, loss):
+        pass
