3aea8b5c0f7992bd67b71cde5fee9fa32a45a01d
qbazilla <qbazilla@hotmail.com>
2020-04-10 12:35:40 +0200

    add saving test results to csv

--- tester/test.py
+++ tester/test.py
@@ -15,3 +15,7 @@ def perform_single_test(config):
     observer = TestObserver(config.validation_set, config.data_collect_freq, config.criterion)
     trainer.train_network(config.nerwork, train_config, observer)
     return observer.get_results()
+
+
+def save_test_results(test_name, results):
+    results.to_csv(f'{test_name}.csv', index=False)
