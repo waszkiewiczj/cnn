a47b9ab7de797673ddf2a08b376133613cc9fbf9
qbazilla <qbazilla@hotmail.com>
2020-04-10 11:51:59 +0200

    move setting seed to helpers

--- helpers.py
+++ helpers.py
@@ -1,5 +1,21 @@
 import torch
+import torch.backends.cudnn
+import numpy as np
+import random
 
 
 def get_device():
     return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def set_seed(seed):
+        """
+        Set seed for all torch func and methods.
+        See ref: https://github.com/pytorch/pytorch/issues/7068
+        """
+        torch.manual_seed(seed)
+        np.random.seed(seed)  # Numpy module.
+        random.seed(seed)  # Python random module.
+        torch.manual_seed(seed)
+        torch.backends.cudnn.benchmark = seed is None
+        torch.backends.cudnn.deterministic = seed is not None
--- trainer/config.py
+++ trainer/config.py
@@ -1,13 +1,11 @@
 import torch
 import torch.utils.data
-import torch.backends.cudnn
-import numpy as np
-import random
+import helpers
 
 
 class TrainConfig:
     def __init__(self, train_set, validation_set, batch_size, epochs, lr, momentum, criterion, seed=None):
-        self.__set_seed(seed)
+        helpers.set_seed(seed)
         self.train_set_loader = torch.utils.data.DataLoader(
             train_set,
             batch_size=batch_size,
@@ -22,15 +20,3 @@ class TrainConfig:
         self.lr = lr
         self.momentum = momentum
         self.criterion = criterion
-
-    def __set_seed(self, seed):
-        """
-        Set seed for all torch func and methods.
-        See ref: https://github.com/pytorch/pytorch/issues/7068
-        """
-        torch.manual_seed(seed)
-        np.random.seed(seed)  # Numpy module.
-        random.seed(seed)  # Python random module.
-        torch.manual_seed(seed)
-        torch.backends.cudnn.benchmark = seed is None
-        torch.backends.cudnn.deterministic = seed is not None
