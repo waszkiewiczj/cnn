d2df77222d9009bdbf41839860a29f1d164311ed
Jakub Waszkiewicz <qbazilla@hotmail.com>
2020-04-11 11:32:57 +0200

    update setting plot ticks

--- tester/plots.py
+++ tester/plots.py
@@ -9,7 +9,7 @@ def create_accuracy_plot(results):
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Accuracy %')
     ax.set_yticks(np.r_[0:100:10])
-    ax.set_xticks(results['epoch'])
+    ax.xaxis.set_major_locator(plt.MaxNLocator(16))
     ax.grid(True, axis='y')
     ax.errorbar(
         x=results['epoch'],
@@ -24,7 +24,7 @@ def create_loss_plot(results):
     ax = fig.add_subplot(111)
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Loss value')
-    ax.set_xticks(results['epoch'])
+    ax.xaxis.set_major_locator(plt.MaxNLocator(16))
     ax.grid(True, axis='y')
     ax.errorbar(
         x=results['epoch'],
