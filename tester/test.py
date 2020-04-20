5a1bcea1330c25395335c36f3c53e1213f65bf54
qbazilla <qbazilla@hotmail.com>
2020-04-21 00:05:14 +0200

    update perform_single_test to use test saver

--- tester/test.py
+++ tester/test.py
@@ -2,6 +2,7 @@ import numpy as np
 import trainer
 import helpers
 from tester.test_observer import TestObserver
+from tester.test_results_saver import TestResultsSaver
 import copy
 
 
@@ -14,9 +15,13 @@ def get_accuracy(predicted, targets):
 def perform_single_test(config):
     observer = TestObserver(freq=config.data_collect_freq)
     train_config = config.to_train_config()
+    saver = TestResultsSaver('test_results')
     for i, seed in enumerate(config.seeds):
         helpers.set_seed(seed)
         network = copy.deepcopy(config.network)
         trainer.train_network(network, train_config, observer)
         print(f'{i + 1}/{len(config.seeds)} test completed')
-    return observer.get_results()
+        saver.save_partial_results(i, observer.get_results())
+    results = observer.get_results()
+    saver.save_full_results(results)
+    return results
