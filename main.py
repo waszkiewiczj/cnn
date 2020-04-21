4415b19170ee4b9543ca40bf262add95db2d2227
qbazilla <qbazilla@hotmail.com>
2020-04-21 09:51:37 +0200

    remove saving test results to variable

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main(config_path):
     with open(config_path, 'r') as file:
         json_config = file.read()
         config = tester.TestConfig.from_json(json_config)
-    _ = tester.perform_single_test(config)
+    tester.perform_single_test(config)
 
 
 if __name__ == '__main__':
