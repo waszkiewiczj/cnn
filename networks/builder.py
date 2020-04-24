3d16abb2bc55923fbbc3f23f030061c324c05813
qbazilla <qbazilla@hotmail.com>
2020-04-24 18:22:54 +0200

    add builder to networks module

--- networks/builder.py
+++ networks/builder.py
@@ -1,55 +1,9 @@
 import torch.nn as nn
-import torch.nn.functional as F
 from torchvision import models
+from networks.hidden import Hidden
+from networks.testnet import TestNet
 
 classes = 10
-
-
-class TestNet(nn.Module):
-    """
-    Network taken from PyTorch repository tutorials.
-    See ref: https://github.com/pytorch/tutorials/blob/master/beginner_source/blitz/cifar10_tutorial.py
-    """
-
-    def __init__(self):
-        super(TestNet, self).__init__()
-        global classes
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, classes)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
-class Hidden(nn.Module):
-
-    def __init__(self, input_size, hidden_sizes, output_size):
-        super(Hidden, self).__init__()
-        if len(hidden_sizes) == 0:
-            raise ValueError('List of hidden layers sizes cannot be empty')
-        self.fc1 = nn.Linear(input_size, hidden_sizes[0])
-        self.fch = [
-            nn.Linear(hidden_sizes[i - 1], hidden_sizes[i])
-            for i in range(1, len(hidden_sizes))
-        ]
-        self.fc2 = nn.Linear(hidden_sizes[-1], output_size)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        for fc in self.fch:
-            x = fc(x)
-        x = self.fc2(x)
-        return x
 
 
 def freeze_parameters(model):
